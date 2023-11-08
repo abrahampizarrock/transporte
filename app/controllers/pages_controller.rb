73c70a1fb76606125b20a160cfefe29046450a12
abrahampizarrock <128652700+abrahampizarrock@users.noreply.github.com>
2023-11-07 22:03:55 -0300

    cards margenes y fondos

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -9,11 +9,29 @@ class PagesController < ApplicationController
   end
 
   def info
+    @direccion = "Puchilco s/n, comuna Puqueldon, Chiloé"
+    @telefono = "telefono"
+    @correo = "correo de la empresa"
+    @facebook = "face de la empresa"
+    @twitter = "twitter de la empresa"
+    @instagram = "insta de la empresa"
   end
 
   def photos
+    @direccion = "Puchilco s/n, comuna Puqueldon, Chiloé"
+    @telefono = "telefono"
+    @correo = "correo de la empresa"
+    @facebook = "face de la empresa"
+    @twitter = "twitter de la empresa"
+    @instagram = "insta de la empresa"
   end
 
   def video
+    @direccion = "Puchilco s/n, comuna Puqueldon, Chiloé"
+    @telefono = "telefono"
+    @correo = "correo de la empresa"
+    @facebook = "face de la empresa"
+    @twitter = "twitter de la empresa"
+    @instagram = "insta de la empresa"
   end
 end
